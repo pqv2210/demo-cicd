e22474441a6dc1a2f177c566e9b37a8f5cd24f80
sphinx-vupq <vupq@sphinx.vn>
2023-07-15 13:40:49 +0700

    remove header

--- App.js
+++ App.js
@@ -66,7 +66,6 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
